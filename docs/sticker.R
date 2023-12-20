6671835ac722f03f369e18857b8ee2ed4458d1a2
Christian Coffman <coffman.christian@epa.gov>
2023-12-20 11:24:58 -0600

    First draft of package logo

--- docs/sticker.R
+++ docs/sticker.R
@@ -1,12 +1,23 @@
 library(tidyverse)
 lakes <- sf::read_sf("Data/Other/ne_10m_lakes.shp")
+
+statline <- data.frame(x = seq(from =0, to =2.2, length.out = 50)) %>%
+  mutate(y =  cos(x) +  rnorm(n = 50, mean=0, sd = 0.1)) %>%
+  mutate(x = (x- min(x))/ (max(x) - min(x)),
+         y = (y- min(y))/ (max(y) - min(y)),
+         x = x * (88 - 84.9) - 88,
+         y = y * (46- 41.7) + 41.7)
+ 
+
 g <- ggplot(lakes) +
+  theme_void() +
   geom_sf(fill = "grey") +
-  coord_sf(xlim = c(-88, -84.9), ylim = c(41.7, 46)) +
-  theme_minimal() + 
-  theme(axis.text = element_blank(),
-        plot.background = element_blank(),
-    panel.background = element_blank() )
+  coord_sf(xlim = c(-88, -84.9), ylim = c(41.7, 46)) + 
+  geom_point(data = statline, aes(x = x, y=y), size= 0.1) +
+  geom_smooth(data = statline, aes(x = x, y=y), color = "#87B13F", size = 0.1)
+
+
+
 
 
 hexSticker::sticker(
@@ -15,6 +26,7 @@ hexSticker::sticker(
   p_size = 20,
   s_width = 1,
   s_height = 1,
+  s_x = 1,
   dpi = 320, filename = "docs/figsTables/logo.png"
 )
 
