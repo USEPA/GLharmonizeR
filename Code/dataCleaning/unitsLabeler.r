0a33f19808d7707de5102f878729225fad50f6ce
Christian Coffman <coffman.christian@epa.gov>
2024-01-03 17:29:28 -0600

    Changing units utility functions

--- Code/dataCleaning/unitsLabeler.r
+++ Code/dataCleaning/unitsLabeler.r
@@ -1,13 +1,41 @@
 library(tidyverse)
 
-# SHORT LIST 
-trouble <- df %>%
-  mutate(above = STN_DEPTH_M < 20) %>%
-  group_by(ANALYTE, FRACTION, METHOD, above) %>%
-  drop_na(VALUE) %>%
-  reframe(d = log10(max(VALUE)/ min(VALUE))) %>%
-  filter(d > 2)
 
+# Convert all units to standard units using the WQX units conversion data table
+#units <- read_csv("https://cdx.epa.gov/wqx/download/DomainValues/MeasureUnit.CSV")
+units <- read_csv("Data/Meta/MeasureUnit.CSV") %>%
+  # remove any annotated with retired
+  filter(!grepl("retired", Description, ignore.case = T)) %>%
+    mutate(Code = tolower(Code),
+    `Target Unit` = tolower(`Target Unit`),
+    # temperature is the only that has extra letters
+    Code = ifelse(grepl("Temperature", Description, ignore.case = T),
+                 str_remove(Code, "deg "), 
+                 Code))
+
+standardizeUnits <- function(df, conversionTable){
+  df %>% 
+    # if no units, relabel with most common units
+    mutate(UNITS = if_else(UNITS == "none", names(sort(table(UNITS), decreasing = T)[1]), UNITS), .by = ANALYTE) %>%
+    # convert to target units
+    left_join(units, by = c( "UNITS" ="Code" )) %>%
+    mutate(VALUE = VALUE * `Conversion Factor`,
+          UNITS = `Target Unit`) %>%
+    # Could consider combining organic carbon units, but haven't yet
+    # detect outliers 
+    mutate(below10 = SAMPLE_DEPTH_M < 10) %>%
+    mutate(p25 = quantile(VALUE, 0.25, na.rm= T),
+           p75 = quantile(VALUE, 0.75, na.rm= T),
+           IQR = p75 - p25,
+           lowerCutoff = p25 - 2* IQR,
+           upperCutoff = p75 + 2 * IQR,
+           outlier = !between(VALUE, lowerCutoff, upperCutoff),
+           .by = c(ANALYTE, below10))
+}
+
+df <- standardizeUnits(df, conversionTable) 
+  group_by(ANALYTE) %>%
+  summarize(mean(outlier, na.rm = T))
 ### ONly check those on the short list
 test <- df %>%
   mutate(VALUE = log10(VALUE + 0.000001), above = STN_DEPTH_M < 20) %>%
@@ -16,14 +44,37 @@ test <- df %>%
   reframe(
           choices = map(list(dist(VALUE)), ~ .x  %>%
               as.matrix() %>%
-              (function(x) colMeans(x >2, na.rm = T))))
+              (function(x) colMeans(x >2, na.rm = T)))
+              )
 
 
+test <- read_csv("Data/Meta/Quantitation Limit Type.csv")
+test <- read_csv("Data/Meta/ResultMeasureQualifier.CSV")
+test <- read_csv("Data/Meta/ResultSampleFraction.CSV")
+test <- read_csv("Data/Meta/Horizontal Collection Method.csv")
+test <- read_csv("Data/Meta/QAQCCharacteristicValidation.CSV")a
+test %>% 
+  filter(grepl("Calcium", Characteristic, ignore.case = T)) %>%
+  select(4,9,10)
 
 df %>%
-  filter(ANALYTE == "Sodium") %>%
-  mutate(VALUE = ifelse(littleUnit, VALUE / 1000, VALUE)) %>%
-  ggplot(aes(x = VALUE)) +
-  geom_histogram()
+  ggplot(aes(x = VALUE)) + 
+  geom_histogram() +
+  scale_x_log10() + 
+  facet_wrap(~ANALYTE, scales= "free")
 
+df %>%
+  summarize(d = dip.test()))
+
+df %>%
+  mutate(VALUE = log10(VALUE + 0.0000001)) %>%
+  group_by(ANALYTE) %>%
+  nest() %>%
+  mutate(test = map(data, ~ diptest::dip.test(.x$VALUE)$statistic))  %>%
+  unnest(test) 
+df %>%
+  ggplot(aes(x = VALUE, ))+
+  geom_histogram() + 
+  geom_text(aes(x = Inf, y = Inf, label = test)) %>%
+  facet_wrap(~ANALYTE, scales = "free")
 
--- Data/Meta/mappingToWQX.r
+++ Data/Meta/mappingToWQX.r
@@ -1,26 +1,46 @@
+source("Code/dataCleaning/readCleanGLENDA.R")
 library(tidyverse)
-library(xml2)
-read_xml("Data/Meta/Characteristic.xml") %>%
-  xml_find_all("//d1:WQXElementRow") %>%
-  lapply(function(x) {
-    xml_children(x) %>%
-      xml_attrs() %>%
-      bind_rows() %>%
-      # filter(colname %in% c("UniqueIdentifier", "Name", "CASNumber", "SRSID", "SampleFractoinRequired", "AnalyticalMethodRequired", "GroupName")) %>%
-      pivot_wider(names_from = "colname", values_from = "value") 
-  }) %>%
-  bind_rows() %>%
-  write_csv(df, "Data/Meta/Characteristic.csv")
+library(gt)
 
-pg %>% 
-  filter(! grepl("retired", Name)) %>% dim()
+df <- readCleanGLENDA("Data/Raw/GLENDA/GLENDA.csv") 
+  filter(grepl("suspended", ANALYTE, ignore.case=T)) %>%
+  select(MEDIUM, ANALYTE, VALUE, METHOD) 
+
+df %>%
+  distinct(ANALYTE, FRACTION, METHOD, UNITS)
+usgs <- read_csv("public_srsnames_July_2023.csv")  
+WQXnicks<- read_csv("Data/Meta/Characteristic Alias.csv") 
+wqx <- read_csv("Data/Meta/Characteristic.csv") 
 
 
-pg %>% 
-  filter(grepl("Alkalin", Name)) %>%
-  select(AnalyticalMethodRequired, MethodSpeciationRequired, SampleFractionRequired)
+wqx %>% 
+  filter(grepl("phosphorus", Name, ignore.case = T)) %>% select(Name) %>% print(n = 33)
+  filter(grepl("ortho", Name, ignore.case = T))  %>% select(Name)
+  filter(UniqueIdentifier == 4017)  %>% 
+  select(5:7)
 
-pg %>%
-  filter(SampleFractionRequired == "Y",
-         MethodSpeciationRequired=="Y") %>%
-  print(n= 36)
+
+
+wqx %>% 
+  filter(`UniqueIdentifier`== 1789) %>%
+  select(SampleFractionRequired,
+   AnalyticalMethodRequired, MethodSpeciationRequired)
+
+wqx <- read_csv("Data/Meta/All Domain Values.csv") %>%
+  filter(Domain == "Characteristic(CharacteristicName)")
+
+
+
+df %>%
+  left_join(WQXnames, by = c("ANALYTE" = "Name")) %>% 
+  select(ANALYTE, FRACTION, METHOD, SampleFractionRequired, AnalyticalMethodRequired, MethodSpeciationRequired) %>%
+  mutate(FRACTION = case_when(
+    FRACTION == "Total/Bulk" ~ "tot",
+    FRACTION == "Residue" ~ "part",
+    FRACTION == "Filtrate" ~ "diss",
+    # The other choices are NA or not applicable
+    .default = ""
+  ))
+
+df2 <- read_csv("Data/Meta/ResultSampleFraction.csv")
+
