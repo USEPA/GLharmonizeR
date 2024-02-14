2073b321de718995902f78c35bd377db7aaeea6f
Christian Coffman <coffman.christian@epa.gov>
2024-02-14 10:29:11 -0600

    Updated roxygen2 version, removed extra GLENDA dataset, reorganized tests to be fully encompassing

--- tests/testthat/test-readNclean.R
+++ tests/testthat/test-readNclean.R
@@ -1,5 +1,28 @@
-test_that("GLENDA data can be read, cleaned and pivoted", {
-  GLENDA <- readCleanGLENDA("Data/Raw/GLENDA/GLENDA.csv") %>%
+
+#%% CSMI
+test_that("CSMI data files can be found, read, cleaned, and joined.", {
+  early <- file.path("L:", "Priv", "Great lakes Coastal", "2002-2010 Water Quality", "2010")
+  t15 <- file.path("L:", "Priv", "Great lakes Coastal", "2015 CSMI Lake Michigan",
+    "WQ data and database", "CSMI data & database", "CSMI_LkMich2015_Database_working_minsRivMouths.accdb")
+  t21 <- file.path("C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)",
+    "Lake Michign ML - General", "Raw_data", "CSMI", "2021")
+
+  files <- c(early, t15, t21)
+  goodFiles <- sum(sapply(files, file.exists))
+  gooddirs<- sum(sapply(files, dir.exists))
+  goodTotal <- goodFiles + gooddirs
+
+  testthat::expect_equal(length(files), goodTotal)
+  CSMI <- LoadCSMI(early, t15, t21)
+  testthat::expect_s3_class(CSMI, "data.frame")
+})
+
+
+#%% GLENDA
+test_that("GLENDA data file can be found, read, cleaned, and pivoted", {
+  filepath <- "Data/Raw/GLENDA/GLENDA.csv"
+  testthat::expect_true(file.exists(filepath))
+  GLENDA <- readCleanGLENDA(filepath) %>%
     rename(UID = SAMPLE_ID, sampleDepth = SAMPLE_DEPTH_M, Depth = STN_DEPTH_M, Comment = RESULT_REMARK, RESULT = VALUE, Date = sampleDate) %>%
     select(UID, Date, Depth, sampleDepth, ANALYTE, RESULT, FRACTION, Comment, UNITS) %>%
     mutate(UID = as.character(UID), RESULT = as.numeric(RESULT),
@@ -7,22 +30,21 @@ test_that("GLENDA data can be read, cleaned and pivoted", {
   testthat::expect_s3_class(GLENDA, "data.frame")
 })
 
-test_that("CSMI data files can loaded, cleaned, and joined", {
-  CSMI <- LoadCSMI(
-    file.path("L:", "Priv", "Great lakes Coastal", "2002-2010 Water Quality", "2010"),
-    file.path("L:", "Priv", "Great lakes Coastal", "2015 CSMI Lake Michigan",
-      "WQ data and database", "CSMI data & database", "CSMI_LkMich2015_Database_working_minsRivMouths.accdb"),
-    file.path("C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)",
-      "Lake Michign ML - General", "Raw_data", "CSMI", "2021"))
-  testthat::expect_s3_class(CSMI, "data.frame")
-})
+#%% NCCA
+test_that("NCCA hydrological data can be found, read, and cleaned", {
+  hydrofile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_hydrographic_profile_great_lakes-data.csv"
+  hydrofile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_hydrographic_profile_great_lakes-data.csv"
+  secchifile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_secchi_great_lakes-data.csv" 
 
-test_that("NCCA hydrological data can be read and cleaned", {
+  nccafiles <- c(hydrofile2015, hydrofile2015, secchifile2015)
+  good_connections <- sum(sapply(nccafiles, valid_url))
+  testthat::expect_equal(length(nccafiles), good_connections)
+
   NCCAhydro <- .readNCCAhydro(
     hydrofiles2010 = c("https://www.epa.gov/sites/default/files/2016-01/assessed_ncca2010_hydrolab.csv", 
   "https://www.epa.gov/sites/default/files/2016-01/not_assessed_ncca2010_hydrolab.csv"),
-    hydrofile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_hydrographic_profile_great_lakes-data.csv",
-    secchifile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_secchi_great_lakes-data.csv" 
+    hydrofile2015 = hydrofile2015,
+    secchifile2015 = secchifile2015 
     ) %>%
     select(UID, DATE_COL, SAMPLE_DEPTH_M, ANALYTE, RESULT, UNITS, STATION_DEPTH_M) %>%
     rename(Date=  DATE_COL, sampleDepth = SAMPLE_DEPTH_M, Depth = STATION_DEPTH_M) %>%
@@ -30,6 +52,7 @@ test_that("NCCA hydrological data can be read and cleaned", {
             STUDY = "NCCA")
   testthat::expect_s3_class(NCCAhydro, "data.frame")
 }) 
+
 
 test_that("NCCA water quality data read and cleaned", {
   siteFiles <- "Data/Raw/NCCA"
@@ -45,4 +68,5 @@ test_that("NCCA water quality data read and cleaned", {
             STUDY = "NCCA")
   testthat::expect_s3_class(nccaWQ, "data.frame")
 })
+
 
