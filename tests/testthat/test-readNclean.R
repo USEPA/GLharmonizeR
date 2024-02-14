6a295707b416b87c737e0aab6c5d53e81f8581cc
Christian Coffman <coffman.christian@epa.gov>
2024-02-13 18:28:45 -0600

    Restructred the testing suite so that the commands that take a long time to check (reading and cleaning) are in their own file

--- tests/testthat/test-readNclean.R
+++ tests/testthat/test-readNclean.R
@@ -1,29 +1,20 @@
-valid_url <- function(url_in,t=1.5){
-  con <- url(url_in)
-  check <- suppressWarnings(try(open.connection(con,open="rt",timeout=t),silent=T)[1])
-  suppressWarnings(try(close.connection(con),silent=T))
-  is.null(check)
-}
-
-# NCCA 2015 hydrographic and secchi
-test_that("NCCA 2015 hydrographic files are accessible", {
-  hydrofile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_hydrographic_profile_great_lakes-data.csv"
-  hydrofile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_hydrographic_profile_great_lakes-data.csv"
-  secchifile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_secchi_great_lakes-data.csv" 
-
-  nccafiles <- c(hydrofile2015, hydrofile2015, secchifile2015)
-  good_connections <- sum(sapply(nccafiles, valid_url))
-  testthat::expect_equal(length(nccafiles), good_connections)
+test_that("GLENDA data can be read, cleaned and pivoted", {
+  GLENDA <- readCleanGLENDA("Data/Raw/GLENDA/GLENDA.csv") %>%
+    rename(UID = SAMPLE_ID, sampleDepth = SAMPLE_DEPTH_M, Depth = STN_DEPTH_M, Comment = RESULT_REMARK, RESULT = VALUE, Date = sampleDate) %>%
+    select(UID, Date, Depth, sampleDepth, ANALYTE, RESULT, FRACTION, Comment, UNITS) %>%
+    mutate(UID = as.character(UID), RESULT = as.numeric(RESULT),
+    STUDY = "GLENDA")
+  testthat::expect_s3_class(GLENDA, "data.frame")
 })
 
-test_that("NCCA Water quality files are accessible", {
-  preFiles <- "Data/Raw/NCCA/nca_waterchemdata.csv"
-  tenFiles<- c("Data/Raw/NCCA/assessed_ncca2010_waterchem.csv", "Data/Raw/NCCA/nassessedWaterChem2010.csv") 
-  fifteenFiles <- "Data/Raw/NCCA/ncca_2015_water_chemistry_great_lakes-data.csv"
-
-  nccafiles <- c(preFiles, tenFiles, fifteenFiles)
-  good_connections <- sum(sapply(nccafiles, file.exists))
-  testthat::expect_equal(length(nccafiles), good_connections)
+test_that("CSMI data files can loaded, cleaned, and joined", {
+  CSMI <- LoadCSMI(
+    file.path("L:", "Priv", "Great lakes Coastal", "2002-2010 Water Quality", "2010"),
+    file.path("L:", "Priv", "Great lakes Coastal", "2015 CSMI Lake Michigan",
+      "WQ data and database", "CSMI data & database", "CSMI_LkMich2015_Database_working_minsRivMouths.accdb"),
+    file.path("C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)",
+      "Lake Michign ML - General", "Raw_data", "CSMI", "2021"))
+  testthat::expect_s3_class(CSMI, "data.frame")
 })
 
 test_that("NCCA hydrological data can be read and cleaned", {
@@ -54,3 +45,4 @@ test_that("NCCA water quality data read and cleaned", {
             STUDY = "NCCA")
   testthat::expect_s3_class(nccaWQ, "data.frame")
 })
+
