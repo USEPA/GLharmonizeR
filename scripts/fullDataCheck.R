5b293820b04b29ea2088deb994e15d17b7429824
Christian Coffman <coffman.christian@epa.gov>
2024-02-21 11:44:06 -0600

    Started an attempt at interfacing with the GLENDA API. And updated file paths for the full data join and check

--- scripts/fullDataCheck.R
+++ scripts/fullDataCheck.R
@@ -6,12 +6,13 @@ NCCAhydrofile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_h
 NCCAsecchifile2015 = "https://www.epa.gov/sites/default/files/2021-04/ncca_2015_secchi_great_lakes-data.csv" 
 
 #NCCA WQ files
-siteFiles <- "Data/Raw/NCCA"
-preFiles <- c("Data/Raw/NCCA/nca_waterchemdata.csv")
-tenFiles<- c("Data/Raw/NCCA/assessed_ncca2010_waterchem.csv", "Data/Raw/NCCA/nassessedWaterChem2010.csv") 
-fifteenFiles <- c("Data/Raw/NCCA/ncca_2015_water_chemistry_great_lakes-data.csv")
+siteFiles <- file.path("Data", "NCCA")
+preFiles <- file.path("Data", "NCCA", "nca_waterchemdata.csv")
+tenFiles<- c(file.path("Data", "NCCA", "assessed_ncca2010_waterchem.csv"),
+            file.path("Data", "NCCA", "nassessedWaterChem2010.csv"))
+fifteenFiles <- file.path("Data", "NCCA", "ncca_2015_water_chemistry_great_lakes-data.csv")
 
-glendaData <- "Data/Raw/GLENDA/GLENDA.csv"
+glendaData <- file.path("Data", "GLENDA.csv")
 
 # L drive data
 # csmi2010 <- file.path("L:", "Priv", "Great lakes Coastal", "2002-2010 Water Quality", "2010")
@@ -22,9 +23,9 @@ glendaData <- "Data/Raw/GLENDA/GLENDA.csv"
 # namingFile <- file.path(
 #   "C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)", "Lake Michigan ML - General", "Results", "Analytes3.xlsx"
 # )
-csmi2010 <- file.path("C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)", "Lake Michigan ML - General", "Raw_data", "CSMI", "2010")
-csmi2015 <- file.path("C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)", "Lake Michigan ML - General", "Raw_data", "CSMI", "CSMI2015_newQuery.accdb")
-csmi2021 <- file.path("C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)", "Lake Michigan ML - General", "Raw_data", "CSMI", "2021")
+csmi2010 <- file.path("Data", "CSMI", "2010")
+csmi2015 <- file.path("Data", "CSMI", "CSMI2015_newQuery.accdb")
+csmi2021 <- file.path("Data", "CSMI", "2021")
 
 namingFile <- file.path("C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)", "Lake Michigan ML - General", "Results", "Analytes3.xlsx")
 
@@ -88,7 +89,7 @@ CSMInames <- allWQ %>%
     Years = max(year(Date), na.rm = T),
     n = n(), 
     .by = c(ANALYTE, FRACTION, ANL_CODE, CodeName, `Lepak input`, Comment))  %>%
-    arrange(ANALYTE)
+    arrange(Years, ANALYTE)
 CSMIkey <- file.path("C:", "Users", "ccoffman", "Environmental Protection Agency (EPA)", "Lake Michigan ML - General", "Results", "Analytes3.xlsx") %>%
   readxl::read_xlsx(path = ., sheet = "CSMI_Map") %>%
   dplyr::mutate(FRACTION = dplyr::case_when(
