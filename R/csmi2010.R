502b09f2ac7019633f27967cc644ce12f83d3055
Christian Coffman <coffman.christian@epa.gov>
2024-03-06 22:06:01 -0600

    Substantial bug fixes to CSMI 2010, but still need to un-break the downstream functions

---
+++ R/csmi2010.R
@@ -0,0 +1,129 @@
+# Should probably check that these data are not redundant with 2010 NCCA data
+
+
+#' Load and join data for CSMI 2010 from csv excel files 
+#'
+#' @description
+#' `.LoadCSMI2010` returns a dataframe of all of the joined water quality data relating to CSMI 2010
+#' 
+#' @details
+#' This is a hidden function, this should be used for development purposes only, users will only call
+#' this function implicitly when assembling their full water quality dataset
+#' @importFrom magrittr "%>%"
+#' @param directorypath a string specifying the directory path of the access database
+#' @return dataframe of the fully joined water quality data from CSMI 2010
+.LoadCSMI2010 <- function(directoryPath){
+  df <- readxl::read_xlsx(file.path(directoryPath, "GL2010db.xlsx")) %>%
+    dplyr::slice(9:n()) %>%
+    # Move spatial information to front to simplify table conversion
+    dplyr::relocate(contains(c("Stn Depth", "Acutal", "Actual"))) %>%
+    dplyr::select(-Notes)
+  sampleCols <- which(grepl("Sample Type", names(df), ignore.case = T))
+  tallFormatted <- df %>% 
+    select(`Stn Depth (m)`:STIS) %>%
+    tidyr::fill(DATE, .direction = "down") %>%
+    dplyr::mutate(DATE = lubridate::dmy(DATE),
+            LATITUDE = as.numeric(`Acutal Lat (N)`),
+            LONGITUDE = as.numeric(`Actual Lon (W)`),
+            STATION_DEPTH = as.numeric(`Stn Depth (m)`)) %>%
+    dplyr::select(-c(`Acutal Lat (N)`, `Actual Lon (W)`, `Stn Depth (m)`, `STIS #...1`))
+
+  
+  # Define where new mini tables are 
+  tableBounds <- lapply((1:(length(sampleCols))), function(i) {
+    df[, sampleCols[i]:
+      ifelse(i + 1 <= length(sampleCols), (sampleCols[i+1] -1),
+                                          (dim(df)[2]))]
+    })
+
+  tallFormatted <- purrr::map(tableBounds,
+    \(df)  df %>%
+            dplyr::rename(FRACTION= 1) %>%
+            # drop STIS
+            dplyr::select(!dplyr::contains("STIS")) %>%
+            dplyr::bind_cols(tallFormatted, .) %>%
+            tidyr::pivot_longer(-c(FRACTION, LAKE, SITE, STATION, PROJECT, DATE, `blk/dup other`, STIS, STATION_DEPTH, LATITUDE, LONGITUDE), names_to = "ANALYTE", values_to = "RESULT")
+            ) %>%
+
+    purrr::reduce(dplyr::bind_rows) %>%
+    dplyr::mutate(RESULT = as.numeric(RESULT),
+                  ANALYTE = stringr::str_remove(ANALYTE, "\\.\\.\\.[[:number:]]*"),
+                  numerator = tolower(stringr::str_extract(ANALYTE, ".g")),
+                  denominator = tolower(stringr::str_extract(ANALYTE, "[[:alpha:]]$")),
+                  UNITS = stringr::str_trim(paste0(numerator, denominator)),
+                  ANALYTE = stringr::str_trim(ifelse(
+                    grepl("Part", ANALYTE, ignore.case =TRUE),
+                    stringr::str_extract(ANALYTE, "^Part[[:blank:]]*."),
+                    stringr::str_extract(ANALYTE, "^[[:graph:]]*")
+                  )),
+                  ANALYTE = ifelse(
+                    grepl("Part  C", ANALYTE, ignore.case = TRUE),
+                    "Part C", 
+                    ANALYTE),
+                  ANALYTE = stringr::str_remove_all(ANALYTE, "\\+"),
+                  ANALYTE = stringr::str_remove_all(ANALYTE, "-"),
+                  ANALYTE = stringr::str_remove_all(ANALYTE, "="),
+                  FRACTION = dplyr::case_when(
+                        FRACTION == "F" ~ "Filtrate",
+                        FRACTION == "U" ~ "Total/Bulk",
+                        FRACTION == "A" ~ "Filtrate",
+                        FRACTION == "M" ~ "Filtrate",
+                        FRACTION == "D" ~ "Filtrate",
+                        FRACTION == "V" ~ "Total/Bulk",
+                        FRACTION == "PCN" ~ "Residue",
+                        FRACTION == "Not applicable" ~ NA,
+                        .default = FRACTION))
+
+
+####### NEEED TO UNBREAK THIS SINCE THE NEW STUFF ADDED ABOVE 
+
+
+    # move detection limits to own column
+  df <- readxl::read_xlsx(file.path(directoryPath, "GL2010db.xlsx")) %>%
+    dplyr::slice(1:2)
+
+  dls <- df %>%
+    dplyr::filter(`STIS#` == "method detection limit") %>%
+    dplyr::distinct(ANALYTE, RESULT) %>% 
+    tidyr::drop_na() %>%
+    dplyr::rename(mdl = RESULT) %>%
+    dplyr::mutate(mdl = as.numeric(mdl))
+
+  df <- df %>%
+    # WHAT IS THIS FILTER DOING? 
+    dplyr::filter(`STIS#` != "STIS #") %>%
+    dplyr::filter(! grepl("detection limit", `STIS#`)) %>%
+    dplyr::left_join(dls, by = "ANALYTE") %>%
+    tidyr::drop_na(RESULT) %>%
+    dplyr::mutate(
+      RESULT = as.numeric(RESULT),
+      Date = lubridate::ymd("2010-06-15")
+      )
+
+
+
+  # Didn't find anything immediately usable in here, maybe it will come up when we do more intense QC
+  # meta1 <- readxl::read_xls(file.path(directoryPath, "LMich10forms.xls"))
+  # meta2 <- readxl::read_xls(file.path(directoryPath, "smpstts10.xls"))
+
+  # CTD data, look like raw CTD measures, where the sheet names might correspond to the site?
+#   ctd <- readxl::excel_sheets(file.path(
+#     "L:",
+#     "Priv",
+#     "Great lakes Coastal",
+#     "2010 MED Lake Michigan",
+#     "CTD data",
+#     "CTD_GB",
+#     "CTD_Casts_2010",
+#     "CTD Casts 2010 Excel.xlsx"
+#   ))
+
+  return(df)
+}
+
+
+df2 %>% 
+
+  distinct(ANALYTE, FRACTION, UNITS) %>%
+  arrange(ANALYTE) %>%
+  write_csv("tempcsmi2010.csv")
